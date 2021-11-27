437ea59d51018e5ae9fd92cf2c5de326fd62431f
PremRajP <90784133+PremRajP@users.noreply.github.com>
2021-11-27 18:48:39 +0530

    A program that would print the information (name, year of joining, salary, address) of three employees by creating a class named 'Employee'.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+package mc;
 class Employee{
 	  private String name, address;
 	  private int year, salary;
@@ -24,6 +25,11 @@ class Employee{
 	public class Main{
 	  public static void main(String[] args){
 	    Employee e1 = new Employee("Robert", 1994, 500000, "Chennai");
+	    Employee e2 = new Employee("Sam", 2000, 740000, "Mumbai");
+	    Employee e3 = new Employee("John", 1999, 600000, "Delhi");
 	    System.out.println("Name\tYear of joining\tSalary\tAddress");
-	    System.out.println(e1.getName()+"\t"+e1.getYear()+"\t\t"+e1.getSalary()+"\t"+e1.getAddress());   }
+	    System.out.println(e1.getName()+"\t"+e1.getYear()+"\t\t"+e1.getSalary()+"\t"+e1.getAddress());  // printing details of employee 1
+	    System.out.println(e2.getName()+"\t"+e2.getYear()+"\t\t"+e2.getSalary()+"\t"+e2.getAddress());  // printing details of employee 2
+	    System.out.println(e3.getName()+"\t"+e3.getYear()+"\t\t"+e3.getSalary()+"\t"+e3.getAddress());  // printing details of employee 3
+	  }
 	}
